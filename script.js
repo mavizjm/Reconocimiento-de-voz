b19accb684228ae53ed5888ce02a6d11c4555428
mavizjm <106202944+mavizjm@users.noreply.github.com>
2023-04-04 15:59:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let rec;
+    if (!("webkitSpeechRecognition" in window)){
+        alert("No podes usar la API");
+    } else{
+        rec = new webkitSpeechRecognition();
+        rec.lang = "es-AR";
+        rec.continuous = true;
+        rec.interim = true
+        rec.addEventListener("result", iniciar);
+    }
+
+function iniciar(event){
+    for(i= event.resultIndex; i < event.results.length; i++){
+        document.getElementById("texto").innerHTML = event.results[i][0].transcript;
+
+    }
+}
+
+rec.start();
